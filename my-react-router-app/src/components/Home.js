129e65b4fc1eb0f30c4d58d11bda4d3d98b29aeb
Santhosh Kumar Rajendran <skr334@nyu.edu>
2020-03-21 09:48:36 -0400

    lesson 40 and copy base for lesson-41

--- my-react-router-app/src/components/Home.js
+++ my-react-router-app/src/components/Home.js
@@ -1,51 +1,42 @@
 import React, { Component} from 'react'
-import axios from 'axios'
 import Pokeball from '../pokeball.png'
+import { connect } from 'react-redux'
 
-class Home extends Component {
-  state = {
-    posts: []
-  }
-
-  componentDidMount(){
-    axios.get('https://jsonplaceholder.typicode.com/posts/')
-      .then(res => {
-        this.setState({
-          posts: res.data
-        });
-      })
-  }
-
-  postList = () => {
-    return (
-      this.state.posts.length ? (
-          this.state.posts.map(post => {
-            return(
-              <div className="card post" key={post.id}>
-                <img src={Pokeball} alt="A pokeball" />
-                <div className="card-content">
-                  <span className="card-title">{post.title}</span>
-                  <p>{post.body}</p>
-                </div>
-              </div>
-            )
-          })
-        ) : (
-          <div className="center">
-            No Posts Yet
+const Home = ({posts}) => {
+  const postList = posts.length ? (
+    posts.map(post => {
+      return(
+        <div className="card post" key={post.id}>
+          <img src={Pokeball} alt="A pokeball" />
+          <div className="card-content">
+            <span className="card-title">{post.title}</span>
+            <p>{post.body}</p>
           </div>
-        )
-    )
-  }
+        </div>
+      )
+    })
+  ) : (
+    <div className="center">
+      No Posts Yet
+    </div>
+  )
 
-  render(){
-    return (
-      <div className="container home">
-        <h4 className="center"> This is the home page </h4>
-        {this.postList()}
-      </div>
-    )
+  return (
+    <div className="container home">
+      <h4 className="center"> This is the home page </h4>
+      {postList}
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return  {
+    posts: [...state.posts, {
+      "id": 1,
+      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    }]
   }
 }
 
-export default Home
+export default connect(mapStateToProps)(Home)
